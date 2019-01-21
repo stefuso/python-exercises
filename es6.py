05ecba2ec48e83044b620b0b49dd2f2e39fb8c36
stefuso <46556949+stefuso@users.noreply.github.com>
2019-01-21 15:56:38 +0100

    Delete es6.py

--- es6.py
+++
@@ -1,5 +0,0 @@
-a=float(input("Inserisci la distanza espressa in Km: "))
-b=float(input("Inserisci la velocita' espressa in Km/h: "))
-t=a/b
-
-print("La distanza verra' percorsa in "+str(t)+" ore.")
