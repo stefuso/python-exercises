bca75caeeb7d49d8f0accd504e7d08eeea8a3818
stefuso <46556949+stefuso@users.noreply.github.com>
2019-01-21 15:56:27 +0100

    Delete es5.py

--- es5.py
+++
@@ -1,6 +0,0 @@
-a=int(input("Inserisci un numero: "))
-b=int(input("Inserisci un numero: "))
-
-print("La somma dei numeri e' pari a %d" %(a+b))
-print("La differenza dei numeri e' pari a %d" %(a-b))
-print("Il prodotto dei numeri e' pari a %d" %(a*b))
